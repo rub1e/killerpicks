3e5acd629d159b8ab1b015b8519ba0d36a1af09b
rub1e <alex.rubner@gmail.com>
2015-07-31 14:07:24 +0100

    login sorted

---
+++ app/client/client-accounts-config.js
@@ -0,0 +1,34 @@
+Accounts.ui.config({
+  requestPermissions: {},
+  passwordSignupFields : "USERNAME_AND_EMAIL",
+  forceEmailLowercase: true,
+  forceUsernameLowercase: true,
+  forcePasswordLowercase: true,
+  extraSignupFields: [{
+    fieldName: 'first-name',
+    fieldLabel: 'First name',
+    inputType: 'text',
+    visible: true,
+    validate: function(value, errorFunction) {
+      if (!value) {
+        errorFunction("Please enter your first name");
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }, {
+    fieldName: 'last-name',
+    fieldLabel: 'Last name',
+    inputType: 'text',
+    visible: true,
+    validate: function(value, errorFunction) {
+      if (!value) {
+        errorFunction("Please enter your last name");
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }]
+});
