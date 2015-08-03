acb75f81973939c5b0ec5175352d9d7362106fc7
rub1e <alex.rubner@gmail.com>
2015-08-03 16:15:38 +0100

    starting on detailview

---
+++ app/client/templates/detailed_league_view/detailed_league_view.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* DetailedLeagueView: Event Handlers */
+/*****************************************************************************/
+Template.DetailedLeagueView.events({
+});
+
+/*****************************************************************************/
+/* DetailedLeagueView: Helpers */
+/*****************************************************************************/
+Template.DetailedLeagueView.helpers({
+});
+
+/*****************************************************************************/
+/* DetailedLeagueView: Lifecycle Hooks */
+/*****************************************************************************/
+Template.DetailedLeagueView.created = function () {
+};
+
+Template.DetailedLeagueView.rendered = function () {
+};
+
+Template.DetailedLeagueView.destroyed = function () {
+};
