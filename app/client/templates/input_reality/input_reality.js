703096f8d4f5bd3c151c6e89ccf24b431d66860a
rub1e <alex.rubner@gmail.com>
2015-08-13 16:49:00 +0100

    added dates to games

--- app/client/templates/input_reality/input_reality.js
+++ app/client/templates/input_reality/input_reality.js
@@ -5,13 +5,20 @@ Template.InputReality.events({
   "submit form" : function(e){
     e.preventDefault();
     var gameWeek = $("#weeksDiv").children()[0].value;
-    var entry = {gameWeek : gameWeek, matches : [], winningTeams : [1,2,3,4,5,6,7,8,9,10]};
+    var entry = {gameWeek : gameWeek, matches : [], winningTeams : ["1","2","3","4","5"]};
+    
     for (var i = 1; i < 11; i += 1){
       var homeT = "#home" + i;
       var awayT = "#away" + i;
-      var obj = {home: $(homeT).val(), away : $(awayT).val()};
+      var gDate = "#gameDate" + i;
+      var gTime = "#gameTime" + i;
+      var obj = {home: $(homeT).val(),
+                 away : $(awayT).val(),
+                 date : $(gDate).val(),
+                 time : $(gTime).val()};
       entry.matches.push(obj);
     }
+
     Meteor.call("inputGames", entry, function(err, res){
       $("#inputReality").find("select").val("");
     });
