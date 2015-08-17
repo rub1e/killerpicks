0fc92a67025e08e6ef7e099d9979700c685d2c69
rub1e <alex.rubner@gmail.com>
2015-08-17 09:39:36 +0100

    relayout working

--- app/client/templates/LeaguesView/leagues_view/leagues_view.js
+++ app/client/templates/LeaguesView/leagues_view/leagues_view.js
@@ -8,19 +8,12 @@ Template.LeaguesView.events({
 /* LeaguesView: Helpers */
 /*****************************************************************************/
 Template.LeaguesView.helpers({
-  "playerLeagues" : function(){
-    console.log("playerLeagues called");
-    return Session.get("leagues");
-  }
 });
 
 /*****************************************************************************/
 /* LeaguesView: Lifecycle Hooks */
 /*****************************************************************************/
 Template.LeaguesView.created = function () {
-  Meteor.call("playerLeaguesArray", function(err, res){
-    Session.set("leagues", res);
-  });
 };
 
 Template.LeaguesView.rendered = function () {
